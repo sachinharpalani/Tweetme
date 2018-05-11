49f8b1bc283e009191503f824ca24b14ad65eb2e
sachin <=>
2018-05-11 13:53:33 +0530

    Added code on validators

--- src/tweets/admin.py
+++ src/tweets/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Tweet
+from .form import TweetModelForm
 
 # Register your models here.
 
-admin.site.register(Tweet)
+class TweetModelAdmin(admin.ModelAdmin):
+    form = TweetModelForm
+    # class Meta:
+    #     model = Tweet
+
+admin.site.register(Tweet, TweetModelAdmin)
--- src/tweets/models.py
+++ src/tweets/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 from django.conf import settings
+from django.core.exceptions import ValidationError
+from .validators import validate_content
 
 # Create your models here.
 
 class Tweet(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-    content = models.CharField(max_length=140)
+    content = models.CharField(max_length=140, validators=[validate_content])
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.content)
+
+    # def clean(self, *args, **kwargs):
+    #     content = self.content
+    #     if content == "abc":
+    #         raise ValidationError("Content Cannot be abc")
+    #     return super(Tweet, self).clean(*args,**kwargs)
